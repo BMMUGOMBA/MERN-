8bea454a67febab6a571f3900808475d77791b2b
Batsirai Mugomba <bmmugomba@gmail.com>
2025-08-14 17:11:16 +0200

    app initial commit:

---
+++ my-react-appConditionalRendering/src/App.jsx
@@ -0,0 +1,9 @@
+//conditional rendering allows you to control what gets rendered in your application
+//based on certainconditions
+//show hide or change components
+
+function App() {
+  
+}
+
+export default App
