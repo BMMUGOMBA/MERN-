7a055c9b34561f56af7164e6b4b2180c07bfbcd8
Batsirai Mugomba <bmmugomba@gmail.com>
2025-08-14 17:31:23 +0200

    render done

--- my-react-appConditionalRendering/src/App.jsx
+++ my-react-appConditionalRendering/src/App.jsx
@@ -2,8 +2,14 @@
 //based on certainconditions
 //show hide or change components
 
+import UserGreeting from './UserGreeting.jsx'
+
 function App() {
-  
+  return(
+    <>
+      <UserGreeting isLoggedIn = {false} username="ProfBatsi"/> 
+    </>
+  )
 }
 
 export default App
